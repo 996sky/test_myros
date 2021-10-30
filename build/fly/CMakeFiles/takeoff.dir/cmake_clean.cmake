0d2a211ec5b2b070edd1c4610bc8953581be5c33
996sky <spacerin@icloud.com>
2021-10-30 07:53:17 -0700

    helloworld运行成功

---
+++ build/fly/CMakeFiles/takeoff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/takeoff.dir/src/takeoff.cpp.o"
+  "/home/rinspace/test_myros/devel/lib/fly/takeoff.pdb"
+  "/home/rinspace/test_myros/devel/lib/fly/takeoff"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/takeoff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/fly/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rinspace/test_myros/src/fly
+# Build directory: /home/rinspace/test_myros/build/fly
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
