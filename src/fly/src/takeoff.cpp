883dcc2e91f945d42a8b01225db7cf12adcbaa92
996sky <spacerin@icloud.com>
2021-10-30 10:52:16 -0700

    mavros运行成功

--- src/fly/src/takeoff.cpp
+++ src/fly/src/takeoff.cpp
@@ -1,13 +1,39 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
+#include <mavros_msgs/State.h>
 
-int main(int argc, char *argv[])
-{
-    //执行 ros 节点初始化
-    ros::init(argc,argv,"hello");
-    //创建 ros 节点句柄(非必须)
-    ros::NodeHandle n;
-    //控制台输出 hello world
+mavros_msgs::State current_state;//当前状态
+
+void callBack(const mavros_msgs::State::ConstPtr& msg) {
+    current_state = *msg;
     ROS_INFO("hello world!");
+    //printf("message get callback!\n");
+}//callBack回调函数
 
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, callBack);
+
+    ros::Rate rate(20.0);//频率为20hz
+
+    while (ros::ok())
+    {
+        ros::spinOnce();//调用一次回调函数
+        rate.sleep();//等待1/20秒
+    }
     return 0;
 }
+
+/*启动命令
+首先
+make px4_sitl_default gazebo
+启动仿真
+
+roscore 启动ROS核心
+打开QGC
+roslaunch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557"
+启动MAVROS
+rosrun 包名 C++节点
+*/
+
